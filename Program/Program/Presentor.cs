68e3f601a10955876d74d0b76c505338d899912c
DepelopProgram19 <56472870+DepelopProgram19@users.noreply.github.com>
2019-10-12 19:45:58 +0500

    Add files via upload

---
+++ Program/Program/Presentor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Presentor
+    {
+        public static int size = 10;
+        public int[] mas = new int[size];
+
+        public Presentor()
+        {
+
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(-100, 101);
+                Console.Write(mas[i] + " ");
+            }
+        }
+
+        public void Add(int []mas, int value)
+        { 
+                int[] newArray = new int[size + 1];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = mas[i];
+            }
+
+            newArray[size+1] = value;
+
+            mas = newArray;
+        }
+
+  
+
+        public void Delete(int []mas,int i)
+        { 
+            Console.WriteLine(string.Join(" ", mas.Select(x => x.ToString()).ToArray()));
+          
+            var query = mas.Where(n => mas.ElementAt(i) != n);
+            Console.WriteLine(string.Join(" ", query.Select(x => x.ToString()).ToArray()));
+            Console.ReadKey();
+
+        }
+
+        public void Show(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+
+                Console.Write(mas[i] + " ");
+
+            }
+        }
+
+        public void SortMas(int []mas)
+        {
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                if (mas[i] % 2==0) continue;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if ((mas[j] % 2!=0) && (mas[i] > mas[j]))
+                    {
+                        int tmp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        private static int[] mas;
+
+        static void Main(string[] args)
+        {
+          
+            Presentor t = new Presentor();
+            Console.WriteLine("Выбирите действие с массивом целых чисел" +
+                              "\n1-Добавить элемент в конец " +
+                               "\n2-Удалить по индексу " +
+                               "\n3-Вывести на экран " +
+                               "\n4-Сортировка по убыванию четных элементов,остальные на местах " +
+                               "\n0-выход из прогораммы  ");
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            switch (k)
+            {
+                case 1:
+                    Console.WriteLine("Введите целое значение");
+                    int f = Console.Read();
+                    t.Add(mas, f);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите индекс элемента который вы хотите удалить");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    t.Delete(mas,n);
+                    break;
+
+                case 3:
+                    t.Show(mas);
+                    break;
+
+                case 4: t.SortMas(mas);
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    break;
+
+            }
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
